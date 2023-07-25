5855f62d3c0ca81a15f9066179a3640b6cd99b63
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-07-25 10:35:51 -0700

    ES6 functions

---
+++ main.js
@@ -0,0 +1,59 @@
+const companies= [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
+  
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+  
+//   for(let i =0 ; i<companies.length;i++){
+//     console.log(companies[i])
+//   }
+
+  //for each
+
+//   companies.forEach(function(company){
+//     console.log(company)
+//   })
+
+//filter
+
+// let canDrink = []
+// for (i=0;i<ages.length;i++){
+//     if(ages[i]>=21){
+//         canDrink.push(ages[i])
+//     }
+// }
+
+// const canDrink = ages.filter(function(age){
+//     if(age>=21){
+//         return true;
+//     }
+// })
+
+// const canDrink = ages.filter(age=>age>=21)
+// console.log(canDrink)
+
+// const retailCompanies = companies.filter(function(company){
+//     if(company.category==='Retail'){
+//         return true
+//     }
+// })
+
+// const retailCompanies = companies.filter(company => company.category === 'Retail')
+// console.log(retailCompanies)
+
+// const eightiesCompanies = companies.filter(company=>company.start<1990 && company.start>1979)
+
+// console.log(eightiesCompanies)
+
+const decadeCompanies=companies.filter(company=>(company.end-company.start)>10)
+
+
+console.log(decadeCompanies)
