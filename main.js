f1e849cf0af71b6c2e7560cb952362e529e05677
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-07-25 21:57:43 -0700

    ES6 function

--- main.js
+++ main.js
@@ -53,7 +53,95 @@ const companies= [
 
 // console.log(eightiesCompanies)
 
-const decadeCompanies=companies.filter(company=>(company.end-company.start)>10)
+// const decadeCompanies=companies.filter(company=>(company.end-company.start)>=10)
 
 
-console.log(decadeCompanies)
+// console.log(decadeCompanies)
+
+//maps
+
+// const testMap=companies.map(function(company){
+//   return `${company.name} [${company.start} - ${company.end}]`
+// })
+
+// const testMap=companies.map(company=>`${company.name} [${company.start} - ${company.end}]`)
+
+// console.log(testMap)
+
+// const agesSquare=ages.map(age=>Math.sqrt(age))
+// console.log(agesSquare)
+
+// const array = [3, 5, 98, 67];
+
+// for (let index = 0; index < array.length; index++) {
+//   console.log(array[index])
+// }
+// array.forEach(item => {
+//   console.log(item)
+// })
+
+// const newArray = array.forEach((item, index) => {
+//   if(item === 3){
+//     return 9;
+//   } else {
+//     return item * 2;
+//   }
+// })
+
+// const newArray = array.filter(item => {
+//   return item < 70;
+// })
+
+// console.log(newArray)
+
+//sort
+
+// const sortedCompanies = companies.sort(function(c1,c2){
+//   if(c1.start>c2.start){
+//     return 1
+//   }else {
+//     return -1
+//   }
+// })
+
+// const sortedCompanies = companies.sort((c1,c2)=>(c1.start>c2.start ? 1 : -1))
+//  const sortedCompanies = companies.sort((c1,c2)=>(c1.start-c2.start))
+
+//  console.log(sortedCompanies)
+
+
+// const sortedAges = ages.sort(function(age1,age2){
+//   if(age1>age2){
+//     return 1;
+//   }else {
+//     return -1;
+//   }
+// })
+// const sortedAges = ages.sort((age1,age2)=>age1-age2)
+ 
+// console.log(sortedAges)
+
+//reduce
+
+// let ageSum =0;
+// for(let i = 0;i<ages.length;i++){
+//   ageSum = ageSum + ages[i]
+// }
+// let ageSum =0;
+// for(let i = 0;i<ages.length;i++){
+//   ageSum += ages[i]
+// }
+
+// console.log(ageSum)
+
+// const totalYears = companies.reduce((total,company)=>total+(company.end-company.start),0
+// )
+
+// console.log(totalYears)
+
+// const combined = ages 
+//   .map(age => age*2)
+//   .filter(age=> age>=40)
+//   .sort((a,b)=>a-b)
+
+//   console.log(combined)
